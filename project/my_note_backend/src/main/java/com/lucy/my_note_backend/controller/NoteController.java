95d2bb4590c79d61332febe81cc7295ffbc63643
BuGwang Cho <kate7443@gmail.com>
2020-03-18 00:19:13 +0900

    200317 mysql연결, 컨트롤러 생성

---
+++ project/my_note_backend/src/main/java/com/lucy/my_note_backend/controller/NoteController.java
@@ -0,0 +1,23 @@
+package com.lucy.my_note_backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NoteController {
+
+    @PostMapping("/note")
+    public String addNote() {
+        return "add note";
+    }
+
+    @PutMapping("/note")
+    public String modifyNote() {
+        return "modify note";
+    }
+
+    @DeleteMapping("/note")
+    public String deleteNote() {
+        return "delete note";
+    }
+
+}
---
+++ project/my_note_backend/src/main/java/com/lucy/my_note_backend/controller/WordController.java
@@ -0,0 +1,32 @@
+package com.lucy.my_note_backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WordController {
+
+    @PostMapping("/word")
+    public String addWord() {
+        return "add word";
+    }
+
+    @PutMapping("/word")
+    public String modifyWord() {
+        return "modify word";
+    }
+
+    @DeleteMapping("/word")
+    public String deleteWord() {
+        return "delete word";
+    }
+
+    @GetMapping("/words")
+    public String getWordList() {
+        return "word list";
+    }
+
+
+
+
+
+}
